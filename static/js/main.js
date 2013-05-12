57b2a4eae7b58673f6aaf97ccf116582a33f47d7
Ivan Gromov <summer.is.gone@gmail.com>
2013-05-13 03:38:57 +0600

    Added more navigation

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,14 @@
+(function($) {
+    "use strict";
+    $(function() {
+        // Highlight menu 
+        $(".nav a").each(function(index, a) {
+            if ($(a).prop("href") === window.location.href) {
+                $(a).parent("li").addClass("active");
+            } else {
+                $(a).parent("li").removeClass("active");
+            }
+        });
 
+    });
+})(jQuery);
